2377c6c1dcd9eef4e0b946af61006295fea69e19
Bianca Hua <biancahua@gmail.com>
2023-01-14 18:29:43 -0800

    updated coinrow component to include detailed columns and delete button

--- src/components/CoinRow.jsx
+++ src/components/CoinRow.jsx
@@ -4,8 +4,6 @@ import '../styles/CoinRow.css'
 export default function CoinRow(props) {
 
   const [ticker, setTicker] = useState(props.ticker);
-
-
   const [mcap, setMcap] = useState(0);
   const [price, setPrice] = useState(0);
   const [sevenDay, setSevenDay] = useState(0);
@@ -14,10 +12,20 @@ export default function CoinRow(props) {
   const [todayChange, setTodayChange] = useState(0);
 
 
+
+  
+
   return (
     <div className="CoinRow">
-
-
+      <table>
+        <td>{props.coin.marketCap}</td>
+        <td>{props.coin.price}</td>
+        <td>{props.coin.sevenDay}</td>
+        <td>{props.coin.thirtyDay}</td>
+        <td>{props.coin.oneYear}</td>
+        <td>{props.coin.TODAY}</td>
+        <button className='delete_btn' onClick={() => props.handleDelete(props.coin.ticker)}>-</button>
+      </table>
     </div>
   )
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,18 +5,20 @@ import Searchbar from '../components/Searchbar'
 import '../styles/Dashboard.css'
 
 export default function Dashboard(props) {
-  
+
   const { user } = props
   
   const [appUser, setAppUser] = useState(user); // user is an object with username and password
-  const [watchlist, setWatchlist] = useState([{ticker: 'BTC'}, {ticker: 'ETH'}, {ticker: 'ADA'}])
+  const [watchlist, setWatchlist] = useState([
+    {ticker: 'BTC', marketCap: '100B', balance: '5,777', price: 0.000038, sevenDay : '+5.1%', thirtyDay: '-27.4%', oneYear: '+1M%', TODAY: '+7.7%'}, 
+    {ticker: 'ETH',  marketCap: '200B', balance: '5,777', price: 0.000038, sevenDay : '+5.1%', thirtyDay: '-27.4%', oneYear: '+1M%', TODAY: '+7.7%'}, 
+    {ticker: 'ADA', marketCap: '300B', balance: '5,777', price: 0.000038, sevenDay : '+5.1%', thirtyDay: '-27.4%', oneYear: '+1M%', TODAY: '+7.7%'}])
+  
 
-  // watchlist = [{ticker: 'BTC'}, {ticker: 'ETH'}, {ticker: 'ADA'}]
-
-  //This deletes the new ticker by creating a new array of tickers without the deleted ticker
   function handleDelete (ticker) {
     setWatchlist(watchlist.filter(coin => coin.ticker !== ticker))
   }
+
 
 
   return (
@@ -30,13 +32,24 @@ export default function Dashboard(props) {
         </div>
       </div>
       <div className="price-chart">
-
+      </div>
+      <div className='header'>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Marketcap</th>
+            <th>Price</th>
+            <th>Seven Day</th>
+            <th>Thirty Day</th>
+            <th>One Year</th>
+            <th>Today Change</th>
+          </tr>
+        </table>
       </div>
       <div className="watchlist">
         {watchlist.map((coin, index) => {
-          return <CoinRow handleDelete={(ticker) => handleDelete(ticker)} ticker={coin.ticker}/>
+          return <CoinRow handleDelete = {(ticker) => handleDelete(ticker)} ticker = {coin.ticker} coin={coin}/>
         })}
-      {/* Object=coin -> key = ticker coin.ticker =*/}
       </div>
       <div className="news-module">
 
