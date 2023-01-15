99ab55ce960e1cb69c84623ddb2de66625bda72a
Bianca Hua <biancahua@gmail.com>
2023-01-14 16:05:02 -0800

    added event handlers for searchbar in the dashboard for input and submission

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Searchbar(props) {
 
-  const { setWatchlist, watchlist} = props
+  const { setWatchlist, watchlist }  = props
 
-  function onSubmitSearch () {
+  //Obtaining the search bar ticker name 
+  const [ tickerName, setTickerName ] = useState("")
+
+  // change tickerName state to input value
+  function onSearchChange (event) {
+    setTickerName(event.target.value)
+  }
+
+  //This adds new ticker to the existing array of tickers 
+  //? coin or ticker?
+  function handleAdd (target) { // fix input, NOT TARGET!!!
+    console.log('invoking handleAdd on search');
+    console.log('tickerName is ', tickerName);
+
     // fetch
-
-
-
-    // setWatchlist([...watchlist], {ticker: data retrieved on success
-  } 
+    
+      fetch('/where we want to go', {
+        method: 'POST',
+        body: JSON.stringify(tickerName),
+        headers: {'Content-Type': 'application/json'}
+        })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response)
+          if(response.ok) {
+            setWatchlist(watchlist.push(response.ticker)) // fix input
+          }
+        })
+      }
+    
 
   return (
     <div className="Searchbar">
-      <input type="text" placeholder='Ticker...' /> <button className='addticker-button'>Add</button>
+      {/* Capture inputed text and set as value of ticker */}
+      <input type="text" placeholder='Ticker...' value={tickerName} onChange={onSearchChange}/> 
+      {/* Upon click event, add new ticker to watchList */}
+      <button onClick={handleAdd} className='addticker-button'>Add</button>
     </div>
   )
 }
+
+// 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,12 +7,13 @@ import '../styles/Dashboard.css'
 export default function Dashboard() {
 
   const [watchlist, setWatchlist] = useState([{ticker: 'BTC'}, {ticker: 'ETH'}, {ticker: 'ADA'}])
-  
 
+  // watchlist = [{ticker: 'BTC'}, {ticker: 'ETH'}, {ticker: 'ADA'}]
+
+  //This deletes the new ticker by creating a new array of tickers without the deleted ticker
   function handleDelete (ticker) {
     setWatchlist(watchlist.filter(coin => coin.ticker !== ticker))
   }
-
 
 
   return (
@@ -30,8 +31,9 @@ export default function Dashboard() {
       </div>
       <div className="watchlist">
         {watchlist.map((coin, index) => {
-          return <CoinRow handleDelete = {(ticker) => handleDelete(ticker)} ticker = {coin.ticker} />
+          return <CoinRow handleDelete={(ticker) => handleDelete(ticker)} ticker={coin.ticker}/>
         })}
+      {/* Object=coin -> key = ticker coin.ticker =*/}
       </div>
       <div className="news-module">
 
