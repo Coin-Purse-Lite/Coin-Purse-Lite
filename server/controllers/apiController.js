51dc5fe120d18d29510244953ad5155aaf236cef
Tanner Malmedal <tannermalmedal@Tanners-MacBook-Pro.local>
2023-01-17 00:35:15 -0800

    got our query to the db to include top 2000 coins

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -9,7 +9,7 @@ apiController.getUserApiData = (req, res, next) => {
 
     //fetch the api, and compare each of our tickers, to each of the api's tickers (they only have 100 coins). If they match, give us the info in an array of objs to send to the front end
       //NOTE: currently both next() calls must be inside of async fetch calls, so that when we reference the data in res.locals at the route in the server, the info is guaranteed to be there when we send the info back to the client
-    axios('http://api.coincap.io/v2/assets')
+    axios('http://api.coincap.io/v2/assets?limit=2000')
     .then(response => {
       res.locals.coinInfo = [];
         userWatchlist.forEach((ticker) => {
